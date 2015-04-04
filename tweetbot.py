0e6251ee8c87b21500c5d66dfdfb3de31cceabe4
Candice McCollough <cmm0004@gmail.com>
2015-04-04 12:46:02 -0400

    dont allow nsfw names to be followed or mentioned

--- tweetbot.py
+++ tweetbot.py
@@ -1,4 +1,4 @@
-import os, oauth2, datetime, time, urllib2, json, tweepy, random
+import os, oauth2, datetime, time, urllib2, json, tweepy, random, re
 
 ########
 # CONSTANTS
@@ -89,19 +89,29 @@ class Follower(object):
         self.most_recent = self.followers[0].screen_name
 
     def _follow_most_recent(self):
-        TWITTER_BOT.create_friendship(screen_name=self.most_recent)
+        if not _check_name_nsfw():
+            TWITTER_BOT.create_friendship(screen_name=self.most_recent)
+        else:
+            print 'nsfw name, didnt follow'
 
     def _am_following(self):
         friendship = TWITTER_BOT.show_friendship(source_screen_name='TeasontheLoose', target_screen_name=self.most_recent)
         #check if friendship[0].following or friendship[0].following_requested are true
-        if friendship[0].following or friendship[0].following_requested:
-            return True
-        return                                                
+        if friendship[0].following:
+            return friendship[0].following
+        elif friendship[0].following_requested:
+            return friendship[0].following_requested
+        return False
                                                  
     def mention_new_follower(self):
-        am_following = self._am_following()        
-        
+        am_following = self._am_following()
+
+        if not self._check_name_nsfw():
+            print "nsfw name, didnt mention"
+            return       
+
         #if not already following the most recent follower:
+        
         if not am_following:
             lines = open("./fixtures/msgs_to_followers.txt").read().splitlines()
             mention = "@" + str(self.most_recent)
@@ -125,7 +135,14 @@ class Follower(object):
                 print "followed new follower " + str(follower.screen_name), datetime.datetime.now()
             except tweepy.TweepError:
                 print 'error, didn\'t follow ' + str(follower.screen_name), datetime.datetime.now()
-                
+    
+    def _check_name_nsfw(self):
+        name = str(self.most_recent)
+        uppercase_name = name.upper()
+        pattern = re.compile("FUCK|DAMN|SHIT|PUSSY|PENIS|COCK|VAGINA|MOTHERFUCK|PISS|FAGGOT")
+        match = pattern.search(name)
+
+        return not not match
 
 if __name__ == "__main__":
     twitter = API()
